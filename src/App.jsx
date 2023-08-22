c0048c0a4474b9850b56fe82741dd4445eb79d48
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-08-22 19:00:23 +0500

    working on main

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,18 @@ function App() {
                 <i className="bi bi-person-fill"></i>
               </span>
             </div>
-            <div className="nameAndTagline"></div>
+            <div className="nameAndTagline">
+              <h1>Ammar Ul Mustafa</h1>
+              <p>MERN STACK DEVELOPER</p>
+              <div>
+                Passionate MERN Stack Developer with a flair for crafting
+                seamless web experiences. Transforming ideas into elegant,
+                functional solutions through innovative coding and a keen eye
+                for design. Eager to contribute expertise in MongoDB, Express,
+                React, and Node.js to create dynamic applications that leave a
+                lasting impact.
+              </div>
+            </div>
           </div>
         </section>
         <section className="sidebar">Sidebar</section>
