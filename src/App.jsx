ba5f890e9a41b17364b9d1fb7e6df1dba0fdc505
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-08-22 16:06:12 +0500

    dsf

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="display">
+        <section className="main">
+          <div className="main1Div">
+            <div className="img">
+              <span>
+                <i className="bi bi-person-fill"></i>
+              </span>
+            </div>
+            <div className="nameAndTagline"></div>
+          </div>
+        </section>
+        <section className="sidebar">Sidebar</section>
+      </div>
+    </>
+  );
+}
+
+export default App;
