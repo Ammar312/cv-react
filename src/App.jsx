8443a4598d8ee802bd1e03b7ffc76a69244931a7
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-08-23 17:41:03 +0500

    almost complete

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,7 @@ function App() {
                 <p>2020-2022</p>
               </div>
               <div className="education">
-                <div>Matriculation in Computer</div>
+                <div>Matriculation in Computer Science</div>
                 <span>The Educators School</span>
                 <p>2018-2020</p>
               </div>
@@ -121,6 +121,108 @@ function App() {
                 <i className="bi bi-github"></i>
               </span>
               <p>github.com/Ammar312</p>
+            </div>
+          </div>
+          <div className="secondBar">
+            <div className="projectDiv">
+              <div className="iconAndHeadingSideBar">
+                <span>
+                  <i className="bi bi-mortarboard"></i>
+                </span>
+                <p>PROJECTS</p>
+              </div>
+              <div className="projects">
+                <div className="project">
+                  <i class="bi bi-app"></i>
+                  <a
+                    href="https://jwt-auth-server-production-8cfa.up.railway.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    JWT Auth Server
+                  </a>
+                </div>
+                <div className="project">
+                  <i class="bi bi-app"></i>
+                  <a
+                    href="https://ammar312.github.io/weather-app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Weather App
+                  </a>
+                </div>
+                <div className="project">
+                  <i class="bi bi-app"></i>
+                  <a
+                    href="https://ammar312.github.io/project-planning/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Trello Clone
+                  </a>
+                </div>
+                <div className="project">
+                  <i class="bi bi-app"></i>
+                  <a
+                    href="https://ammar312.github.io/grocery-list/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Grocery Bud
+                  </a>
+                </div>
+                <div className="project">
+                  <i class="bi bi-app"></i>
+                  <a
+                    href="https://ammar312.github.io/threads-clone/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Threads Clone
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="projectDiv">
+              <div className="iconAndHeadingSideBar">
+                <span>
+                  <i class="bi bi-translate"></i>
+                </span>
+                <p>LANGUAGES</p>
+              </div>
+              <div className="projects">
+                <div className="project">
+                  <i class="bi bi-app"></i>
+                  <p className="language">URDU</p>
+                </div>
+                <div className="project">
+                  <i class="bi bi-app"></i>
+                  <p className="language">ENGLISH</p>
+                </div>
+              </div>
+            </div>
+            <div className="projectDiv">
+              <div className="iconAndHeadingSideBar">
+                <span>
+                  <i class="bi bi-translate"></i>
+                </span>
+                <p>HOBBIES</p>
+              </div>
+              <div className="projects">
+                <div className="project">
+                  <i class="bi bi-app"></i>
+                  <p className="language">Learning</p>
+                </div>
+                <div className="project">
+                  <i class="bi bi-app"></i>
+                  <p className="language">Travelling</p>
+                </div>
+                <div className="project">
+                  <i class="bi bi-app"></i>
+                  <p className="language">Gardening</p>
+                </div>
+              </div>
             </div>
           </div>
         </section>
